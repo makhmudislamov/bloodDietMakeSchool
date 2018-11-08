480c96f882d5487e1cdf0503a7862b446cc9851e
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-11-08 11:43:57 -0800

    mongoose installed and index page is up

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 // declaring express and installing
 const express = require('express')
 const app = express()
-
+// mongoose
+const mongoose = require('mongoose');
 // adding exp-hbs
 var exphbs = require('express-handlebars');
 
 // using handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+// connecting to db via mongoose
+mongoose.connect('mongodb://localhost/dietPlatform');
+
+
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+    { title: "Great Review", movieTitle: "Batman II" },
+    { title: "Awesome Movie", movieTitle: "Titanic" }
+]
+
 
 // process.env.PORT >> remote port and 3000 >> local
 const port = process.env.PORT || 3000
 
 // INDEX route
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!' });
+    res.render('reviews-index', { reviews: reviews });
+    // res.render('home', { msg: 'Handlebars are Cool!' });
 })
 
 app.listen(port, () => {
