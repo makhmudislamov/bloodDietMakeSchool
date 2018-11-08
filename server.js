e3566c38c2831321bdb139d737ca088a56f48414
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-11-08 11:25:16 -0800

    added handlebars

--- server.js
+++ server.js
@@ -2,6 +2,13 @@
 const express = require('express')
 const app = express()
 
+// adding exp-hbs
+var exphbs = require('express-handlebars');
+
+// using handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 // process.env.PORT >> remote port and 3000 >> local
 const port = process.env.PORT || 3000
 
