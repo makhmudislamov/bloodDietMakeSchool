1494bf0af0093a4f8f7ee81bd0c945f7b1ba2197
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-11-28 22:11:58 -0800

    create route

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // declaring express and installing
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 // mongoose
 const mongoose = require('mongoose');
 // adding exp-hbs
 var exphbs = require('express-handlebars');
 // process.env.PORT >> remote port and 3000 >> local
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // using handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -20,7 +23,7 @@ const BloodType = mongoose.model("BloodType", {
     fullName: String,
     weight: String,
     height: String,
-    types: String
+    bloodTypes: String
 })
 
 
@@ -29,42 +32,51 @@ const BloodType = mongoose.model("BloodType", {
 app.get('/', (req, res) => {
     // res.render('reviews-index', { reviews: reviews });
     res.render('home');
-})
+});
 
 // SHOW FORM 
 app.get('/blood/new', (req, res) => {
     // res.render('reviews-index', { reviews: reviews });
     res.render('main-form');
-})
+});
 
 
 // TODO: create route for new input
 // same name as the values  
-app.post('/blood/:type/diet')
+app.post('/blood', (req, res) => {
+    BloodType.create(req.body).then((bloodtype) => {
+        console.log(bloodtype);
+        console.log(req.body)
+        res.redirect('/');
+    }).catch((err) => {
+        console.log(err.message);
+    })
+    
+});
 
 // 
-// CREATE
+// 
 app.get('/blood/:type/diet', (req, res) => {
     // call the func
-})
+});
 
 // SHOW
 app.get('/blood/:type/diet/:id', (req, res) => {
     // res.render('home')
     console.log("working")
-})
+});
 
-function showDiet() {
-    var select = document.getElementsByName("selectBlood").value
-        if (select == "A")
-            alert("yahhoooo type A diet is here")
-            // res.render("")
+// function showDiet() {
+//     var select = document.getElementsByName("selectBlood").value
+//         if (select == "A")
+//             alert("yahhoooo type A diet is here")
+//             // res.render("")
 
-}
+// }
 
 
 
 
 app.listen(port, () => {
     console.log('App listening on port 3000!')
-})
+});
